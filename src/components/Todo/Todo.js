39ccba9a44a002e72b101992858dd900c845d69b
Anthony P. Tran <atranp@gmail.com>
2022-06-27 12:50:26 -0400

    Build out logic for removing todos

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,16 +1,26 @@
 import React, {useState} from 'react'
 import TodoForm from '../TodoForm/TodoForm'
-import TodoList from '../TodoList/TodoList'
+import { RiCloseCircleLine} from 'react-icons/ri'
+import { TiEdit} from 'react-icons/ti'
 import './todo.css'
 
-const Todo = () => {
+const Todo = ({todos, completeTodo, removeTodo}) => {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
     })
 
   return todos.map((todo, index) => (
-    <div className={todo.isComplate ? 'todo-row complate' : 'todo-row'} key={index}></div>
+    <div className={todo.isComplate ? 'todo-row complate' : 'todo-row'} key={index}>
+
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+            {todo.text}
+        </div>
+        <div className='icons'>
+            <RiCloseCircleLine onClick={() => removeTodo(todo.id)} className='delete-icon' />
+            <TiEdit onClick={() => setEdit({id: todo.id, value: todo.text} )} className='edit-icon' />
+        </div>
+    </div>
   ))
 }
 
